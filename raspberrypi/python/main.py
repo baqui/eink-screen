0262b026928f8def220812afb0bf853a411b3c9c
Wojciech <wojtek@qunabu.com>
2018-02-13 22:29:33 +0100

    altered carbon image

--- raspberrypi/python/main.py
+++ raspberrypi/python/main.py
@@ -38,18 +38,18 @@ def main():
     epd.init()
 
     # For simplicity, the arguments are explicit numerical coordinates
-    image = Image.new('L', (EPD_WIDTH, EPD_HEIGHT), 255)    # 255: clear the frame
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
-    draw.rectangle((0, 6, 640, 40), fill = 127)
-    draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
-    draw.rectangle((200, 80, 600, 280), fill = 127)
-    draw.chord((240, 120, 580, 220), 0, 360, fill = 255)
-    draw.rectangle((20, 80, 160, 280), fill = 0)
-    draw.chord((40, 80, 180, 220), 0, 360, fill = 127)
-    epd.display_frame(epd.get_frame_buffer(image))
+    # image = Image.new('L', (EPD_WIDTH, EPD_HEIGHT), 255)    # 255: clear the frame
+    # draw = ImageDraw.Draw(image)
+    # font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+    # draw.rectangle((0, 6, 640, 40), fill = 127)
+    # draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
+    # draw.rectangle((200, 80, 600, 280), fill = 127)
+    # draw.chord((240, 120, 580, 220), 0, 360, fill = 255)
+    # draw.rectangle((20, 80, 160, 280), fill = 0)
+    # draw.chord((40, 80, 180, 220), 0, 360, fill = 127)
+    # epd.display_frame(epd.get_frame_buffer(image))
 
-    image = Image.open('640x384.bmp')
+    image = Image.open('640x384xCarbon.bmp')
     epd.display_frame(epd.get_frame_buffer(image))
 
     # You can get frame buffer from an image or import the buffer directly:
