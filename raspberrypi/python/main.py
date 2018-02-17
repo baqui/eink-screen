32b12d63e1ebab0a44ab0cec6df0bfa5426c2907
wojciech baklazec <w.baklazec@gmail.com>
2018-02-17 23:23:38 +0100

    first skm display test

--- raspberrypi/python/main.py
+++ raspberrypi/python/main.py
@@ -28,6 +28,8 @@ import epd7in5b
 import Image
 import ImageDraw
 import ImageFont
+import csv
+import os
 #import imagedata
 
 EPD_WIDTH = 640
@@ -37,32 +39,78 @@ def main():
     epd = epd7in5b.EPD()
     epd.init()
 
-    # For simplicity, the arguments are explicit numerical coordinates
+    skm_timetable_file = os.path.abspath("/home/pi/eink-screen/raspberrypi/python/skm.csv")
     image = Image.new('L', (EPD_WIDTH, EPD_HEIGHT), 255)    # 255: clear the frame
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 55)
-    draw.text((200, 100), 'I', font = font, fill = 0)
-    draw.text((200, 200), 'LOVE', font = font, fill = 0)
-    draw.text((200, 300), 'DICKS', font = font, fill = 0)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 20)
+
+    col_w = 60
+    col_h = 39
+    cols = 3
+    rows = 6
+
+    cal_w = 1 + ((col_w + 1) * cols)
+    cal_h = 1 + ((col_h + 1) * rows)
+
+    cal_x = EPD_WIDTH - cal_w - 2
+    cal_y = 2
+
+    # Paint out a black rectangle onto which we'll draw our canvas
+    draw.rectangle((cal_x, cal_y, cal_x + cal_w - 1, cal_y + cal_h - 1), fill=0, outline=255)
+
+
+    # Draw the vertical lines which separate the columns
+    # and also draw the day names into the table header
+    for x in range(cols):
+        # Figure out the left edge of the column
+        o_x = (col_w + 1) * x
+        o_x += cal_x
+        # Offset to the right side of the column and draw the vertical line
+        o_x += col_w + 1
+        draw.line((o_x, cal_y, o_x, cal_h), fill=255)
+
+    # Draw the horizontal lines which separate the rows
+    for y in range(rows):
+        o_y = (col_h + 1) * y
+        o_y += cal_y + col_h + 1
+        draw.line((cal_x, o_y, cal_w + cal_x - 1, o_y), fill=255)
+
+    skm_data = []
+
+    skm_timetable = open(skm_timetable_file, 'r')
+    skm_reader = csv.reader(skm_timetable, delimiter='\t', lineterminator='\n')
+
+    for row in skm_reader:
+        train_time = row[0].split(':')
+        skm_data.append([train_time[0], train_time[1], row[1]])
+
+    skm_from = 0
+    skm_to = skm_from + rows
+    closest_trains = skm_data[skm_from:skm_to]
+
+    def print_digit(position, digit, colour):
+
+        o_x, o_y = position
+        draw.text((o_x, o_y), str(digit), font = font, fill = colour)
+
+    def print_number(position, number, colour):
+
+        for digit in str(number):
+            print_digit(position, int(digit), colour)
+            position = (position[0] + 8, position[1])
+
+
+    for row, train in enumerate(closest_trains):
+        y = (col_h + 1) * row
+        y += cal_y + 1
+
+        for col, number in enumerate(train):
+            x = (col_w + 1) * col
+            x += cal_x + 1
+            number = number if int(number) >= 10 else '0' + str(int(number))
+            print_number((x+3, y+3), str(number), 127 if col == 2 else 255)
+
     epd.display_frame(epd.get_frame_buffer(image))
-
-
-    # image = Image.new('L', (EPD_WIDTH, EPD_HEIGHT), 255)    # 255: clear the frame
-    # draw = ImageDraw.Draw(image)
-    # font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 55)
-    # draw.rectangle((0, 6, 640, 40), fill = 127)
-    # draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
-    # draw.rectangle((200, 80, 600, 280), fill = 127)
-    # draw.chord((240, 120, 580, 220), 0, 360, fill = 255)
-    # draw.rectangle((20, 80, 160, 280), fill = 0)
-    # draw.chord((40, 80, 180, 220), 0, 360, fill = 127)
-    # epd.display_frame(epd.get_frame_buffer(image))
-
-    # image = Image.open('640x384xCarbon.bmp')
-    # epd.display_frame(epd.get_frame_buffer(image))
-
-    # You can get frame buffer from an image or import the buffer directly:
-    #epd.display_frame(imagedata.MONOCOLOR_BITMAP)
 
 if __name__ == '__main__':
     main()
---
+++ raspberrypi/python/skm-checker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import csv
+import datetime
+import os
+
+skm_data = []
+skm_timetable_file = os.path.abspath("/home/pi/eink-screen/raspberrypi/python/skm.csv")
+
+with open(skm_timetable_file,'r') as f1:
+    reader = csv.reader(f1, delimiter='\t',lineterminator='\n',)
+
+    #read skm data
+    for row in reader:
+        data_row = row[:]
+        train_time = row[0].split(':')
+        if datetime.datetime.now().time() <= datetime.time(hour=int(train_time[0]), minute=int(train_time[1])):
+            skm_data.append(data_row)
+
+
+with open(skm_timetable_file,'w') as f1:
+    writer = csv.writer(f1, delimiter='\t',lineterminator='\n',)
+
+    #write data to csv
+    for index, el in enumerate(skm_data):
+        writer.writerow(el)
